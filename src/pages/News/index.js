d62445898b46c545fd57ea34338fb6418b379d0a
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-12-25 17:41:52 +0300

    feat(news page): add Feature and loader

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import headerImg from './assets/img/news-header.jpg';
+import { Main, Feature, Loader } from '../../components';
 
 const News = () => {
-    return <div>News</div>;
+    return (
+        <>
+            <Loader active={false} full={false} />
+            <Feature {...{ img: headerImg, heading: 'Новости', narrow: true }} />
+            <Main>News</Main>
+        </>
+    );
 };
 
 export default News;
