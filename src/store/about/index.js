da8d6e61c64b8226ddc0d58b25eda87d32d39193
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-12-26 10:02:19 +0300

    feat(about store): add store logic

---
+++ src/store/about/index.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import { kea } from 'kea';
+
+const app = window.app;
+
+export default kea({
+    path: () => ['scenes', 'about'],
+
+    actions: () => ({
+        setLoading: (data) => data,
+        setAboutData: (data) => data
+    }),
+
+    reducers: ({ actions }) => ({
+        isLoading: [
+            false,
+            PropTypes.bool,
+            {
+                [actions.setLoading]: (_, payload = true) => payload
+            }
+        ],
+        aboutData: [
+            {},
+            PropTypes.object,
+            {
+                [actions.setAboutData]: (state, payload) => ({ ...state, ...payload })
+            }
+        ]
+    }),
+    thunks: ({ actions }) => ({
+        getAboutData: async () => {
+            actions.setLoading();
+
+            await app.content
+                .get('about')
+                .then(({ article, cover: [coverId] = [], title }) => {
+                    actions.setAboutData({ article, title });
+                    app.storage
+                        .getURL(coverId, {
+                            size: {
+                                width: 'device'
+                            }
+                        })
+                        .then((cover) => actions.setAboutData({ cover }));
+                })
+                .catch((e) => {
+                    console.error(e);
+                });
+
+            actions.setLoading(false);
+        }
+    })
+});
