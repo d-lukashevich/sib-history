de424e6e303d522fdf60017382ccbcc1e243292d
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-12-26 10:10:54 +0300

    feat(about page): add page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const About = () => <div>About</div>;
+import { useActions, useValues } from 'kea';
+
+import { Main, Feature, Loader, Article } from '../../components';
+
+import { createMarkup } from '../../utils';
+
+import aboutLogic from '../../store/about';
+
+const About = () => {
+    const {
+        aboutData: { article, title, cover },
+        isLoading
+    } = useValues(aboutLogic);
+    const { getAboutData } = useActions(aboutLogic);
+
+    useEffect(() => {
+        getAboutData();
+    }, [getAboutData]);
+
+    return (
+        <>
+            <Loader active={isLoading} full={!article && !title && !cover} />
+            <Feature {...{ img: cover, heading: title, narrow: true }} />
+            <Main>
+                <Article>
+                    <div dangerouslySetInnerHTML={createMarkup(article)} />
+                </Article>
+            </Main>
+        </>
+    );
+};
 
 export default About;
