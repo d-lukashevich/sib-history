e1eac55de59a0bbcc5f77833c37ef8fa8c92e838
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-12-26 15:38:31 +0300

    feat(Feature): add video lightbox

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Lightbox from 'fslightbox-react';
+
+import { useLightboxController } from '../../utils';
+
 import {
     StyledFeature,
     StyledInner,
@@ -9,22 +13,32 @@ import {
     StyledDescription
 } from './units';
 
-const Feature = ({ heading, tag, img, href, description, narrow = false, video = false, double = false }) => (
-    <StyledFeature {...{ img, narrow, double }}>
-        <StyledInner {...{ img, narrow, video, href, double, as: href ? 'a' : 'div' }}>
-            {(heading || description) && (
-                <StyledDetails {...{ double }}>
-                    {(tag || video) && (
-                        <StyledTagLine>
-                            <StyledTag>{tag || 'видео'}</StyledTag>
-                        </StyledTagLine>
+const Feature = ({ heading, tag, img, href, description, narrow = false, video = false, double = false, ...rest }) => {
+    const [lightboxState, openImgSlide] = useLightboxController();
+    return (
+        <>
+            {video && <Lightbox key={href} type={'youtube'} {...lightboxState} sources={[href]} />}
+            <StyledFeature {...{ img, narrow, double, ...rest }}>
+                <StyledInner
+                    {...{ img, narrow, video, href, double, as: href ? 'a' : 'div' }}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        openImgSlide(href);
+                    }}>
+                    {(heading || description) && (
+                        <StyledDetails {...{ double }}>
+                            {(tag || video) && (
+                                <StyledTagLine>
+                                    <StyledTag>{tag || 'видео'}</StyledTag>
+                                </StyledTagLine>
+                            )}
+                            {heading && <StyledHeading>{heading}</StyledHeading>}
+                            {description && <StyledDescription>{description}</StyledDescription>}
+                        </StyledDetails>
                     )}
-                    {heading && <StyledHeading>{heading}</StyledHeading>}
-                    {description && <StyledDescription>{description}</StyledDescription>}
-                </StyledDetails>
-            )}
-        </StyledInner>
-    </StyledFeature>
-);
-
+                </StyledInner>
+            </StyledFeature>
+        </>
+    );
+};
 export default Feature;
